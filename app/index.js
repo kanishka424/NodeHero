1c95607f3fd7fb7ae5c6457cfa6d603c9930df26
kanishka424 <kanishkamalpana24@gmail.com>
2021-01-25 10:44:07 +0530

    adding error  middleware to express

--- app/index.js
+++ app/index.js
@@ -2,22 +2,16 @@ const express =require ('express')
 
 const app =express()
 
-app.use((request,response,next)=>{
-    console.log(request.headers)
-    next()
-})
-
-app.use((request,response,next)=>{
-    request.chance=Math.random()
-    next()
-})
-
-
-
 app.get('/',(request,response)=>{
-    response.json({
-        chance: request.chance
-    })
+    throw new Error('oops')
+
+})
+
+
+app.use((err,request,response,next)=>{//error handler middleware
+    console.log(err)
+    response.status(500).send('Something broke!')
+
 })
 
 app.listen(3000)
