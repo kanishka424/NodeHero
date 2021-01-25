608f1f9124c9affbb6cd01bad4daff6fae9471d9
kanishka424 <kanishkamalpana24@gmail.com>
2021-01-25 10:40:33 +0530

    adding middleware to express

--- app/index.js
+++ app/index.js
@@ -2,20 +2,45 @@ const express =require ('express')
 
 const app =express()
 
-const port =3000
-
-app.get('/',(request,response)=>{
-    response.send('Hello from Express!')
+app.use((request,response,next)=>{
+    console.log(request.headers)
+    next()
 })
 
-app.listen(port,(err)=>{
-    if(err){
-        return console.log('something bad happened',err)
-    }
+app.use((request,response,next)=>{
+    request.chance=Math.random()
+    next()
+})
 
-    console.log(`server is listening on ${port}`)
-}
-)
+
+
+app.get('/',(request,response)=>{
+    response.json({
+        chance: request.chance
+    })
+})
+
+app.listen(3000)
+
+
+// const express =require ('express')
+
+// const app =express()
+
+// const port =3000
+
+// app.get('/',(request,response)=>{
+//     response.send('Hello from Express!')
+// })
+
+// app.listen(port,(err)=>{
+//     if(err){
+//         return console.log('something bad happened',err)
+//     }
+
+//     console.log(`server is listening on ${port}`)
+// }
+// )
 
 
 
