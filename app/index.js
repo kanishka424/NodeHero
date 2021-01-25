49e3436291db923836a1006aea888cc46fdd336c
kanishka424 <kanishkamalpana24@gmail.com>
2021-01-25 10:16:56 +0530

    basic node server with http and Express added2

--- app/index.js
+++ app/index.js
@@ -1,19 +1,40 @@
-const http = require('http')
+const express =require ('express')
+
+const app =express()
+
 const port =3000
 
+app.get('/',(request,response)=>{
+    response.send('Hello from Express!')
+})
 
-const requestHandler = (request,response)=>{
-    console.log(request.url)
-    response.end('Hello Node.js server')
-}
-
-
-const server = http.createServer(requestHandler)
-
-server.listen(port,(err)=>{
+app.listen(port,(err)=>{
     if(err){
         return console.log('something bad happened',err)
     }
 
     console.log(`server is listening on ${port}`)
-})
+}
+)
+
+
+
+// const http = require('http')
+// const port =3000
+
+
+// const requestHandler = (request,response)=>{
+//     console.log(request.url)
+//     response.end('Hello Node.js server')
+// }
+
+
+// const server = http.createServer(requestHandler)
+
+// server.listen(port,(err)=>{
+//     if(err){
+//         return console.log('something bad happened',err)
+//     }
+
+//     console.log(`server is listening on ${port}`)
+// })
